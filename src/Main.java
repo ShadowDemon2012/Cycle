ffc9cc32125b4c875fa683442c7384982fb9c64e
Dmitriy <shadowgods@mail.ru>
2022-07-19 23:55:40 +0500

    третье задание(коррекция)

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int cometFly = 79;
         int currentYear = 2022;
-        for (int i = 0; i < 2100; i = i + cometFly) {
+        for (int i = 0; i < currentYear+100; i = i + cometFly) {
             if (currentYear - 200 < i) {
                 System.out.println(i);
             }
