713f96467996adc3c5844f6a23a963c8ae97facf
Dmitriy <shadowgods@mail.ru>
2022-07-19 21:46:02 +0500

    Второе задание

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-    int i=1;
-        while (i<=10){
-            System.out.print(i +" ");
-            i++;
-        }
-        System.out.print("\n");
-        for (int c=10;c>0;c--)
-        {
-            System.out.print(c+" ");
-        }
+    int firstFriday=3;
+
+    for (;firstFriday<=31;firstFriday=firstFriday+7)
+    {
+        System.out.println("Сегодня пятница, "+ firstFriday+"-е число. Небходимо подготовить отчет");
+    }
+
     }
 }
