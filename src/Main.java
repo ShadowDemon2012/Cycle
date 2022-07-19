e435f8aa6a657f3ed5dbc95763bf7bb941388e01
Dmitriy <shadowgods@mail.ru>
2022-07-19 23:11:12 +0500

    наводим красоту в третьем задании

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-    int cometFly=79;
-    int currentYear=2022;
-    for (int i=0;i<2100;i=i+cometFly)
-    {if (currentYear-200<i)
-    {System.out.println(i);}
-    }
+        int cometFly = 79;
+        int currentYear = 2022;
+        for (int i = 0; i < 2100; i = i + cometFly) {
+            if (currentYear - 200 < i) {
+                System.out.println(i);
+            }
+        }
     }
 }
