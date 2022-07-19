1d3745e97b672b02a6ecd692f60e325913d6372e
Dmitriy <shadowgods@mail.ru>
2022-07-19 23:04:16 +0500

    Четвертое задание(Ping Pong)

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-    int cometFly=79;
-    int currentYear=2022;
-    for (int i=0;i<2100;i=i+cometFly)
-    {if (currentYear-200<i)
-    {System.out.println(i);}
+    for (int i=1;i<=30;i++)
+    {
+        if (i % 3==0 && i % 5==0)
+        {System.out.println(i+": Ping Pong");}
+         else if (i % 3==0) {System.out.println(i+": Ping");}
+            else if (i % 5==0) {System.out.println(i+": Pong");}
+                    else {System.out.println(i+":");}
     }
-    }
+
+                                                            }
 }
